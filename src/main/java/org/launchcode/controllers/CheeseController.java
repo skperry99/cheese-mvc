d5c1004f8da32bd918f37c88055ca208c2c5db6d
Sarah Perry <sperry939@gmail.com>
2017-04-03 18:45:11 -0500

    Added "Add Cheese" form, template , and controller to dynamically create list

--- src/main/java/org/launchcode/controllers/CheeseController.java
+++ src/main/java/org/launchcode/controllers/CheeseController.java
@@ -3,6 +3,10 @@ package org.launchcode.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 /**
  * Created by Sarah on 4/3/2017.
@@ -11,11 +15,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("cheese")
 public class CheeseController {
 
+    static ArrayList<String> cheeses = new ArrayList<>();
+
+    // Request path: /cheese
     @RequestMapping(value = "")
     public String index(Model model) {
 
-
+        model.addAttribute("cheeses", cheeses);
         model.addAttribute("title", "My Cheeses");
+
         return "cheese/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddCheeseForm(Model model) {
+        model.addAttribute("title", "Add Cheese");
+        return "cheese/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddCheeseForm(@RequestParam String cheeseName){
+        cheeses.add(cheeseName);
+
+        //Redirect to /cheese
+        return "redirect:";
     }
 }
